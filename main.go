0bd7f67204cc4119e8c80392d0727d108a8f9416
Linden Brochu <lindenb477@hotmail.com>
2025-02-13 08:04:36 -0500

    Add a compilable main.go

--- main.go
+++ main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-    "custom-lang/src"
-    "flag"
-    "log"
+	"custom-lang/src"
+	"flag"
+	"log"
 )
 
 func main() {
-    var filename *string
-    filename = flag.String("main", "", "Name of the main file")
-    var dryRun *bool
-    dryRun = flag.Bool("dryRun", false, "Only load code, useful to see errors")
+	var filename *string
+	filename = flag.String("main", "", "Name of the main file")
+	var dryRun *bool
+	dryRun = flag.Bool("dryRun", false, "Only load code, useful to see errors")
 
-    flag.Parse()
-    if *filename == "" {
-        log.Fatal("You must provide a file name\n")
-    }
+	flag.Parse()
+	if *filename == "" {
+		log.Fatal("You must provide a file name\n")
+	}
 
-    var err error = src.LoadModule(*filename)
+	//var program *execution.Program
+	var err error
+	_, err = src.LoadModule(*filename)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if *dryRun {
-        log.Println("Dry-run mode enabled")
-    }
+	if !*dryRun {
+		// TODO : Run program here
+	}
 }
